36493c34f9d24eb770a5272c115960446a77f8df
Anastasia <avkozlova8@gmail.com>
2024-11-22 22:02:36 +0300

    1. Исправлено отображение страницы профиля 2. Исправлено отображение фотографии профиля 3. Добавлены отступы справа для userName, userNickname и userText 4. Убран лишний отступ на экране ленты

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -12,16 +12,16 @@ final class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let userPicture = UIImage(named: "userPicture")
+        let userPicture = UIImage(named: "UserPicture")
         let avatarImage = UIImageView(image: userPicture)
-        self.view.backgroundColor = .ypBlack
+        view.backgroundColor = .ypBlack
         
-        self.view.addSubview(avatarImage)
+        view.addSubview(avatarImage)
         avatarImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            avatarImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-            avatarImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 76),
+            avatarImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            avatarImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
             avatarImage.widthAnchor.constraint(equalToConstant: 70),
             avatarImage.heightAnchor.constraint(equalToConstant: 70)
         ])
@@ -32,11 +32,12 @@ final class ProfileViewController: UIViewController {
         userName.font = .boldSystemFont(ofSize: 23)
         
         view.addSubview(userName)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        userName.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             userName.leadingAnchor.constraint(equalTo: avatarImage.leadingAnchor),
             userName.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 8),
+            userName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
         
         let userNickname = UILabel()
@@ -45,11 +46,13 @@ final class ProfileViewController: UIViewController {
         userNickname.font = .systemFont(ofSize: 13)
         
         view.addSubview(userNickname)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        userNickname.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             userNickname.leadingAnchor.constraint(equalTo: avatarImage.leadingAnchor),
             userNickname.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8),
+            userNickname.trailingAnchor.constraint(equalTo: userName.trailingAnchor)
+
         ])
         
         let userText = UILabel()
@@ -59,11 +62,13 @@ final class ProfileViewController: UIViewController {
         userText.numberOfLines = 0
         
         view.addSubview(userText)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        userText.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             userText.leadingAnchor.constraint(equalTo: avatarImage.leadingAnchor),
             userText.topAnchor.constraint(equalTo: userNickname.bottomAnchor, constant: 8),
+            userText.trailingAnchor.constraint(equalTo: userName.trailingAnchor)
+
         ])
         
         
@@ -71,10 +76,10 @@ final class ProfileViewController: UIViewController {
         exitButton.setImage(UIImage(named: "ExitSymbol"), for: .normal)
         
         view.addSubview(exitButton)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            exitButton.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 99),
+            exitButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 99),
             exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
         ])
         
